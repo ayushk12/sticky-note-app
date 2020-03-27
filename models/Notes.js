318760efa8151eda1097a172cc654a59bd103a41
ayush kumar <kayush242@gmail.com>
2020-03-28 00:14:56 +0530

    timestamp for note

--- models/Notes.js
+++ models/Notes.js
@@ -1,21 +1,26 @@
 // creating Schema for  notes
 const mongoose = require("mongoose");
 
-const noteSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    default: ""
+const noteSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      default: ""
+    },
+    description: {
+      type: String,
+      default: ""
+    },
+    user: {
+      ref: "user",
+      type: mongoose.Schema.Types.ObjectId
+    },
+    files: [{ type: String }]
   },
-  description: {
-    type: String,
-    default: ""
-  },
-  user: {
-    ref: "user",
-    type: mongoose.Schema.Types.ObjectId
-  },
-  files: [{ type: String }]
-});
+  {
+    timestamps: true
+  }
+);
 
 const Note = mongoose.model("note", noteSchema);
 module.exports = Note;
